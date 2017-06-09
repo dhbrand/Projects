2ecab3a165d642b53b113fa30d6351a6b4542f19
Dave Hiltbrand <davehiltbrand@gmail.com>
2017-06-09 12:23:10 -0400

    Daily Update
    Reference 6/9 in notes and changes to Shiny Apps

--- Visualization/ShinyApps/winnow-app/app.R
+++ Visualization/ShinyApps/winnow-app/app.R
@@ -18,42 +18,59 @@ ui = fluidPage(
             fluidRow(
                 selectInput("plot_type", 
                             label = "Choose a plot to display",
-                            choices = c("plot1", "ggplot2+geom_line"),
+                            choices = c("geom_point", "geom_line"),
                             selected = "base"),
                 selectInput("var1", 
-                            label = "Choose first variable to display",
-                            choices = c("tp", "fp", "tpr", "fpr","error","accurary"),
+                            label = "Choose x-axis variable to display",
+                            choices = c("tp", "fp", "tpr", "fpr","error","accuracy"),
                             selected = "tp"),
                 selectInput("var2", 
-                            label = "Choose second variable to display",
-                            choices = c("tp", "fp", "tpr", "fpr","error","accurary"),
+                            label = "Choose y-axis variable to display",
+                            choices = c("tp", "fp", "tpr", "fpr","error","accuracy"),
                             selected = "fp")
                       )
                   ),
       mainPanel(
-              plotOutput("plot1")
-                ),
-              br(),br(),
-              tableOutput("results")
-              )
-      
-            ),
+            tabsetPanel(
+              tabPanel("Plot",plotOutput("plot1")),
+              tabPanel("Table",
+                
+              fluidRow(
+                column(4,
+                       h4("means"),
+                       tableOutput("meanresults")
+                       ),
+                column(4,
+                      h4("sums"),
+                      tableOutput("sumresults")
+                      ),
+                column(4,
+                      h4("means"),
+                      tableOutput("meanresults2")
+                      )
+                    )
+              )    
+            )#ends tabsetPanel
+          )#ends mainPanel
+      )#ends sidebarLayout
+  ),#end ui = fluidPage
 
 
 server = function(input, output) {
   output$plot1 = renderPlot({
-    if (input$plot_type == "plot1") {
+    if (input$plot_type == "geom_point") {
       ggplot(dat,aes_string(x=input$var1,y=input$var2))+geom_point(color="firebrick")
-    } else if (input$plot_type == "ggplot2") {
+    } else if (input$plot_type == "geom_line") {
       ggplot(dat,aes_string(x=input$var1,y=input$var2))+geom_line(size = 1, alpha = 1 )+
         labs(title= "Comparison of Rates")
     }
   })
   
-  output$results = renderTable({
-    sum(dat$tp)
-    
-  })
+  output$meanresults = renderTable({apply(dat[,1:4],2,mean)},rownames=TRUE,caption=paste("test"))
+  
+  output$sumresults = renderTable(apply(dat[,5:8],2,sum),rownames=TRUE)
+  
+  output$meanresults2 = renderTable(apply(dat[,9:17],2,mean),rownames=TRUE)
   
   
   
---
+++ Visualization/shinyapps scratch-work script.R
@@ -0,0 +1,35 @@
+library(ROCR)
+library(ggplot2)
+data(ROCR.simple)
+pred <- prediction( dat$predictions, ROCR.simple$labels)
+perf <- performance(pred,"tpr","fpr")
+plot(perf)
+
+dat=read.table("winnowTestResults.txt", header=T)
+print(str(dat))
+
+plot(dat$accuracy,dat$error)
+lines(dat$accuracy,dat$error)
+str(dat)
+data.frame(dat) 
+
+
+ggplot(dat,aes(dat$fpr,dat$tpr,color="red"))+geom_line(size = 1, alpha = 1)+
+  labs(title= "ROC curve", 
+       x = "False Positive Rate (1-Specificity)", 
+       y = "True Positive Rate (Sensitivity)")
+
+##loading xtable to renderTable function using data frame
+install.packages("xtable")
+library(xtable)
+dat.table=xtable(dat)
+
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='dhbrand',
+                          token='16CA732EDC8A8D27B1219F9F8F1E394F',
+                          secret='<SECRET>')
+rsconnect::setAccountInfo(name='dhbrand',
+                          token='16CA732EDC8A8D27B1219F9F8F1E394F',
+                          secret='u17gUfsPXdLfN84PpMIlA/Y5UHloLjA4SVF1WUnd')
+library(rsconnect)
+rsconnect::deployApp('/tooltip-ggplot2-app')
