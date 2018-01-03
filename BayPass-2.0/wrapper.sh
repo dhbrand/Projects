b5b3ea1877f6f94b3074f4380bb0c3119e0fa0ba
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-01-03 09:58:20 -0500

    Added BayPass app that published to Agave.

--- BayPass-2.0/wrapper.sh
+++ BayPass-2.0/wrapper.sh
@@ -28,7 +28,7 @@ else
 fi
 if [ -z "${scalecov}" ]; then
 	echo "scale covariates not set"
-else
+else 
 	echo "scale covariates set to ${scalecov}"
 	ARGS="${ARGS} -scalecov ${scalecov}"
 fi
@@ -63,144 +63,156 @@ else
 	echo "Estimation of Pi beta prior active"
 	ARGS="${ARGS} -setpibetaprior ${setpibetaprior}"
 fi
-if [ -z "${betapiprior}" ]; then
-	echo "Pi beta prior parameters not set"
-else
-	echo "Pi beta prior parameters set to ${betapiprior}"
-	ARGS="${ARGS} -betapiprior ${betapiprior}"
+if [ -z "${betapipriormin}" ]; then
+	echo "Pi beta prior minimum parameter not set"
+else 
+	echo "Pi beta prior minimum parameter set to ${betapipriormin}"
+	ARGS="${ARGS} -betapipriormin ${betapipriormin}"
+fi
+if [ -z "${betapipriormax}" ]; then
+	echo "Pi beta prior maximum parameter not set"
+else 
+	echo "Pi beta prior maximum parameter set to ${betapipriormax}"
+	ARGS="${ARGS} -betapipriormax ${betapipriormax}"
 fi
 if [ -z "${minbeta}" ]; then
 	echo "Lower beta coefficients not set"
-else
+else 
 	echo "Lower beta coefficients set to ${minbeta}"
 	ARGS="${ARGS} -minbeta ${minbeta}"
 fi
 if [ -z "${maxbeta}" ]; then
 	echo "Upper beta coefficients not set"
-else
+else 
 	echo "Upper beta coefficients set to ${maxbeta}"
 	ARGS="${ARGS} -maxbeta ${maxbeta}"
 fi
 ##IS Covariate mode
 if [ -z "${nbetagrid}" ]; then
 	echo "IS covariate mode grid points not set"
-else
+else 
 	echo "IS covariate mode grid points set to ${nbetagrid}"
 	ARGS="${ARGS} -nbetagrid ${nbetagrid}"
 fi
 ##MCMC Covariate mode
 if [ -z "${covmcmc}" ]; then
 	echo "MCMC covariate mode not activated"
-else
+else 
 	echo "MCMC covariate mode activated"
 	ARGS="${ARGS} -covmcmc ${covmcmc}"
 fi
 if [ -z "${auxmodel}" ]; then
 	echo "Auxiliary variable mode not activated"
-else
+else 
 	echo "Auxiliary model mode activated"
 	ARGS="${ARGS} -auxmodel ${auxmodel}"
 fi
 if [ -z "${isingbeta}" ]; then
 	echo "Beta of the Ising model not set"
-else
+else 
 	echo "Beta of the Ising model set to ${isingbeta}"
 	ARGS="${ARGS} -isingbeta ${isingbeta}"
 fi
-if [ -z "${auxPbetaprior}" ]; then
-	echo "auxiliary P beta prior parameters not set"
-else
-	echo "auxiliary P beta prior parameters set to ${auxPbetaprior}"
-	ARGS="${ARGS} -auxPbetaprior ${auxPbetaprior}"
+if [ -z "${auxPbetapriormin}" ]; then
+	echo "auxiliary P beta prior minimum parameter not set"
+else 
+	echo "auxiliary P beta prior minimum parameter set to ${auxPbetapriormin}"
+	ARGS="${ARGS} -auxPbetapriormin ${auxPbetapriormin}"
+fi
+if [ -z "${auxPbetapriormax}" ]; then
+	echo "auxiliary P beta prior maximum parameter not set"
+else 
+	echo "auxiliary P beta prior maximum parameter set to ${auxPbetapriormax}"
+	ARGS="${ARGS} -auxPbetapriormax ${auxPbetapriormax}"
 fi
 ##MCMC Options
 if [ -z "${nthreads}" ]; then
 	echo "Number of threads for MCMC not set"
-else
+else 
 	echo "Number of threads for MCMC set to ${nthreads}"
 	ARGS="${ARGS} -nthreads ${nthreads}"
 fi
 if [ -z "${nval}" ]; then
 	echo "Number of post-burnin and thinned samples to generate for MCMC not set"
-else
+else 
 	echo "Number of post-burnin and thinned samples to generate for MCMC set to ${nval}"
 	ARGS="${ARGS} -nval ${nval}"
 fi
 if [ -z "${thin}" ]; then
 	echo "Size of thining for MCMC not set"
-else
+else 
 	echo "Size of thining for MCMC set to ${thin}"
 	ARGS="${ARGS} -thin ${thin}"
 fi
 if [ -z "${burnin}" ]; then
 	echo "Burn-in length for MCMC not set"
-else
+else 
 	echo "Burn-in length for MCMC set to ${burnin}"
 	ARGS="${ARGS} -burnin ${burnin}"
 fi
 if [ -z "${npilot}" ]; then
 	echo "Number of pilot runs for MCMC not set"
-else
+else 
 	echo "Number of pilot runs for MCMC set to ${npilot}"
 	ARGS="${ARGS} -npilot ${npilot}"
 fi
 if [ -z "${pilotlength}" ]; then
 	echo "Number of pilot runs for MCMC not set"
-else
+else 
 	echo "Number of pilot runs for MCMC set to ${pilotlength}"
 	ARGS="${ARGS} -pilotlength ${pilotlength}"
 fi
 if [ -z "${accinf}" ]; then
 	echo "Lower target acceptance rate bound for MCMC not set"
-else
+else 
 	echo "Lower target acceptance rate bound for MCMC set to ${accinf}"
 	ARGS="${ARGS} -accinf ${accinf}"
 fi
 if [ -z "${accsup}" ]; then
 	echo "Upper target acceptance rate bound for MCMC not set"
-else
+else 
 	echo "Upper target acceptance rate bound for MCMC set to ${accsup}"
 	ARGS="${ARGS} -accsup ${accsup}"
 fi
 if [ -z "${adjrate}" ]; then
 	echo "Adjustment factor for MCMC not set"
-else
+else 
 	echo "Adjustment factor for MCMC set to ${adjrate}"
 	ARGS="${ARGS} -adjrate ${adjrate}"
 fi
 if [ -z "${adjrate}" ]; then
 	echo "Adjustment factor for MCMC not set"
-else
+else 
 	echo "Adjustment factor for MCMC set to ${adjrate}"
 	ARGS="${ARGS} -adjrate ${adjrate}"
 fi
 if [ -z "${d0pi}" ]; then
 	echo "Initial delta for the pi all. freq. proposal for MCMC not set"
-else
+else 
 	echo "Initial delta for the pi all. freq. proposal for MCMC set to ${d0pi}"
 	ARGS="${ARGS} -d0pi ${d0pi}"
 fi
 if [ -z "${upalphaalt}" ]; then
 	echo "Alternative update of the pij for MCMC not set"
-else
+else 
 	echo "Alternative update of the pij for MCMC set"
 	ARGS="${ARGS} -upalphaalt ${upalphaalt}"
 fi
 if [ -z "${d0pij}" ]; then
 	echo "Initial delta for the pij all. freq. proposal for MCMC not set"
-else
+else 
 	echo "Initial delta for the pij all. freq. proposal for MCMC set to ${d0pij}"
 	ARGS="${ARGS} -d0pij ${d0pij}"
 fi
 if [ -z "${d0yij}" ]; then
 	echo "Initial delta for the yij all. freq. proposal for MCMC not set"
-else
+else 
 	echo "Initial delta for the yij all. freq. proposal for MCMC set to ${d0yij}"
 	ARGS="${ARGS} -d0yij ${d0yij}"
 fi
 if [ -z "${seed}" ]; then
 	echo "Random Number Generator for MCMC not set"
-else
+else 
 	echo "Random Number Generator for MCMC set to ${seed}"
 	ARGS="${ARGS} -seed ${seed}"
 fi
