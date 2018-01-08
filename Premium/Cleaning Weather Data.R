8947d97ed2083edee3649d49a6e2e5b522b5eec5
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-01-08 14:03:41 -0500

    Find ways to analyze missing variables

--- Premium/Cleaning Weather Data.R
+++ Premium/Cleaning Weather Data.R
@@ -1,21 +1,46 @@
+library(dplyr)
+library(plyr)
+
+setwd("~/Stapleton_Lab/Projects/Premium/R work/")
 
 ##import cleaned weather dataset
-dataset <- read.csv("~/Stapleton_Lab/Downloads/Carolyn_Lawrence_Dill_G2F_Mar_2017/c._2015_weather_data/g2f_2015_weather_clean.csv")
+df <- read.csv("~/Stapleton_Lab/Downloads/Carolyn_Lawrence_Dill_G2F_Mar_2017/c._2015_weather_data/g2f_2015_weather_clean.csv")
 
-##grabbing all the different experiment IDs
-expnum <- unique(dataset$`Experiment(s)`)
+##renaming variables
+df1 <- rename(df, c("Record.Number"="recnum", "Station.ID"="station", "Experiment.s."="exp", "Day.of.Year"="daynum",
+                "Time..Local."="timelocal", "Datetime..UTC."="timeUTC", "Temperature..C."="temp", 
+                "Dew.Point..C."="dew", "Relative.Humidity...."="relhum", "Solar.Radiation..W.m2."="solrad",
+                "Rainfall..mm."="rain", "Wind.Speed..m.s."="windspeed", "Wind.Direction..degrees."="winddir",
+                "Wind.Gust..m.s."="windgust", "Soil.Temperature..C."="soiltemp", "Soil.Moisture...."="soilmoist"))
 
-##cleaning variable names
-temp <- dataset$`Temperature [C]`
-dew <- dataset$`Dew Point [C]`
-relativehum <- dataset$`Relative Humidity [%]`
-solarrad <- dataset$`Solar Radiation [W/m2]`
-rain <- dataset$`Rainfall [mm]`
-windspeed <- dataset$`Wind Speed [m/s]`
-winddir <- dataset$`Wind Direction [degrees]`
-windgust <- dataset$`Wind Gust [m/s]`
-soiltemp <- dataset$`Soil Temperature [C]`
-soilmoist <- dataset$`Soil Moisture [%]`
+##check missing values
+#install.packages("mice")
+library(mice)
+md.pattern(df[,c(10,14)])
+
+
+library(Hmisc)
+impute(df1$temp, mean)  # replace with mean
+md.pattern
+
+##replace missing values with Random Forest
+miceMod <- mice(df[, !names(df) %in% df[10]], method="rf")
+df2 <- complete(miceMod)
+##replacing missing weather obs with knnImputation 
+##install.packages("DMwR")
+library(DMwR)
+knnOutput <- knnImputation(df[, !names(df) %in% df[10]])  # perform knn imputation.
+anyNA(df[10])
+
+##group by experiment and day
+regrp <- dataset %>% group_by(Experiment.s.)
+group_by(dataset, Experiment.s.) %>% summarize(m = mean(dataset$Temperature..C.))
+res.plyr <- ddply( dataset, .(dataset$Experiment.s.,dataset$eachday), function(x) mean(x$temp) )
+
+
+
+##easier to call variables
+attach(dataset)
 
 ##weather variables
 ##need diff method
