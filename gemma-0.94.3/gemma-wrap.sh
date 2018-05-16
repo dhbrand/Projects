0e271cf0108683866640a3989450c4fbcb953f48
Dave Hiltbrand <davehiltbrand@gmail.com>
2018-05-16 12:52:13 -0400

    changed all scripts

--- gemma-0.94.3/gemma-wrap.sh
+++ gemma-0.94.3/gemma-wrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 set -x
 chmod +x gemma
 inputBED=${inputBED}
@@ -25,7 +25,7 @@ output=${output}
 PLINKfileset=${inputBED%.*}
 #Variable Validation for PLINK and BIMBAM formats:
 
-if [ "$PLINK" -eq 1 ] ; then
+if [[ "$PLINK" -eq 1 ][] ; then
 	if [[ ! -e "$inputBED" ]]; then
 		echo "ERROR: InputBED file was not found" >&2
 		exit 1
@@ -36,7 +36,7 @@ if [ "$PLINK" -eq 1 ] ; then
 		echo "ERROR: InputFAM file was not found" >&2
 		exit 1
 	fi
-elif [ "$BIMBAM" -eq 1 ] ; then
+elif [[ "$BIMBAM" -eq 1 ]] ; then
 	if [[ ! -e "$MeanGenotype" ]]; then
 		echo "ERROR: MeanGenotype file for BIMBAM format was not found" >&2
 		exit 1
@@ -59,46 +59,46 @@ echo "output: $output"
 echo "PLINKfileset: $PLINKfileset"
 #Check the type of analysis to be done
 
-if [ "$Type" -eq 1 ] ; then
+if [[ "$Type" -eq 1 ]] ; then
 	if [[ "$PLINK" -eq 1 ]] ; then
 		gemma -bfile "$PLINKfileset" -gk "$gk" -o "$output"
 	elif [[ "$BIMBAM" -eq 1 ]] ; then
 		gemma -g "$MeanGenotype" -p "$Phenotype" -gk "$gk" -o "$output"
 	fi
-elif [ "$Type" -eq 2 ] ; then
-	if [ "$PLINK" -eq 1 ] ; then
+elif [[ "$Type" -eq 2 ]] ; then
+	if [[ "$PLINK" -eq 1 ]] ; then
 		gemma -bfile "$PLINKfileset" -k "$RelatednessMatrix" -eigen -o "$output"
-	elif [ "$BIMBAM" -eq 1 ] ; then
+	elif [[ "$BIMBAM" -eq 1 ]] ; then
 		gemma -g "$MeanGenotype" -p "$Phenotype" -k "$RelatednessMatrix" -eigen -o "$output"
-	fi	
-elif [ "$Type" -eq 3 ] ; then
-	if [ "$PLINK" -eq 1 ] ; then
+	fi
+elif [[ "$Type" -eq 3 ]] ; then
+	if [[ "$PLINK" -eq 1 ]] ; then
 		gemma -bfile "$PLINKfileset" -k "$RelatednessMatrix" -lmm "$lmm" -o "$output"
-	elif [ "$BIMBAM" -eq 1 ] ; then
+	elif [[ "$BIMBAM" -eq 1 ]] ; then
 		if [[ -n "$Annotation" ]] ; then
 			gemma -g "$MeanGenotype" -p "$Phenotype" -a "$Annotation" -k "$RelatednessMatrix" -lmm "$lmm" -o "$output"
 		else
 			gemma -g "$MeanGenotype" -p "$Phenotype" -k "$RelatednessMatrix" -lmm "$lmm" -o "$output"
 		fi
 	fi
-elif [ "$Type" -eq 4 ] ; then
-	if [ "$PLINK" -eq 1 ] ; then
+elif [[ "$Type" -eq 4 ]] ; then
+	if [[ "$PLINK" -eq 1 ]] ; then
 		gemma -bfile "$PLINKfileset" -k "$RelatednessMatrix" -num "$PhenotypeNumbers" -lmm "$lmm" -o "$output"
-	elif [ "$BIMBAM" -eq 1 ] ; then
+	elif [[ "$BIMBAM" -eq 1 ]] ; then
 		if [[ -n "$Annotation" ]] ; then
 			gemma -g "$MeanGenotype" -p "$Phenotype" -a "$Annotation" -k "$RelatednessMatrix" -num "$PhenotypeNumbers" -lmm "$lmm" -o "$output"
 		else
 			gemma -g "$MeanGenotype" -p "$Phenotype" -k "$RelatednessMatrix" -num "$PhenotypeNumbers" -lmm "$lmm" -o "$output"
 		fi
 	fi
-elif [ "$Type" -eq 5 ] ; then
-	if [ "$PLINK" -eq 1 ] ; then
+elif [[ "$Type" -eq 5 ]] ; then
+	if [[ "$PLINK" -eq 1 ]] ; then
 		gemma -bfile "$PLINKfileset" -bslmm "$bslmm" -o "$output"
-	elif [ "$BIMBAM" -eq 1 ] ; then
+	elif [[ "$BIMBAM" -eq 1 ]] ; then
 		if [[ -n "$Annotation" ]] ; then
 			gemma -g "$MeanGenotype" -p "$Phenotype" -a "$Annotation" -bslmm "$bslmm" -o "$output"
 		else
 			gemma -g "$MeanGenotype" -p "$Phenotype" -bslmm "$bslmm" -o "$output"
 		fi
 	fi
-fi	
+fi
